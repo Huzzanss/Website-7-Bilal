621cd6491f1c6576917f1bff0d20839ae8f85a8b
Hafidzzz <huzzans.hafidz@gmail.com>
2026-07-25 16:34:03 +0800

    Fix V0.0.1

--- page/loginadminpanel/login.js
+++ page/loginadminpanel/login.js
@@ -1,4 +1,5 @@
 document.addEventListener("DOMContentLoaded", () => {
+  // Jika sudah login, langsung lempar ke admin panel
   if (sessionStorage.getItem("adminAuth") === "true") {
     window.location.href = "../adminpanel/";
     return;
